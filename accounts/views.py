18e9cb633962863d01538a0c4578e0fa8ea01b46
jschan0911@naver.com <jschan0911@naver.com>
2022-06-28 16:10:15 +0900

    first commit

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from accounts.models import myUser
 
 def login(request):
     #POST 요청이 들어오면 로그인 처리를 해줌
-    if request.method == 'POST': 
+    if request.method == 'POST':
         userid = request.POST['username']
         pwd = request.POST['password']
         user = auth.authenticate(request, username=userid, password=pwd) 
